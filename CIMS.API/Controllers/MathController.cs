54b70642d40574b392d7c48936da67f0205a7c29
Shirin <Shirin0789@hotmail.com>
2021-10-30 16:44:48 +0300

    Added new service file

--- CIMS.API/Controllers/MathController.cs
+++ CIMS.API/Controllers/MathController.cs
@@ -11,12 +11,6 @@ namespace CIMS.API.Controllers
     [ApiController]
     public class MathController : ControllerBase
     {
-        /// <summary>
-        /// Add two numbers
-        /// </summary>
-        /// <param name="a"></param>
-        /// <param name="b"></param>
-        /// <returns></returns>
         [HttpPost]
         [Route("Addition")]
         public decimal Addition(decimal a, decimal b)
@@ -24,12 +18,6 @@ namespace CIMS.API.Controllers
             return a + b;
         }
 
-        /// <summary>
-        /// Subtract two numbers
-        /// </summary>
-        /// <param name="a"></param>
-        /// <param name="b"></param>
-        /// <returns></returns>
         [HttpPost]
         [Route("Subtraction")]
         public decimal Subtraction(decimal a, decimal b)
@@ -37,12 +25,6 @@ namespace CIMS.API.Controllers
             return a - b;
         }
 
-        /// <summary>
-        /// Multiply two numbers
-        /// </summary>
-        /// <param name="a"></param>
-        /// <param name="b"></param>
-        /// <returns></returns>
         [HttpPost]
         [Route("Multiplication")]
         public decimal Multiplication(decimal a, decimal b)
@@ -50,12 +32,6 @@ namespace CIMS.API.Controllers
             return a * b;
         }
 
-        /// <summary>
-        /// Devide two numbers
-        /// </summary>
-        /// <param name="a"></param>
-        /// <param name="b"></param>
-        /// <returns></returns>
         [HttpPost]
         [Route("Division")]
         public decimal Division(decimal a, decimal b)
---
+++ CIMS.Service/IMathService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIMS.Service
+{
+    public interface IMathService
+    {
+        public decimal Addition(decimal a, decimal b);
+
+        public decimal Subtraction(decimal a, decimal b);
+
+        public decimal Multiplication(decimal a, decimal b);
+
+        public decimal Division(decimal a, decimal b);
+    }
+}
