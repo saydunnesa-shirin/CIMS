98c639f7066dac659594a46cbf234737d674f7c8
Shirin <Shirin0789@hotmail.com>
2021-10-31 14:00:09 +0200

    Constractor added to test

--- CIMS.Service.Test/MathServiceTests.cs
+++ CIMS.Service.Test/MathServiceTests.cs
@@ -4,26 +4,26 @@ namespace CIMS.Service.Test
 {
     public class MathServiceTests
     {
-
         [SetUp]
         public void Setup()
         {
         }
 
-        //private readonly IMathService _mathService;
-        //public MathServiceTests(IMathService mathService)
-        //{
-        //    _mathService = mathService;
-        //}
+        private readonly IMathService mathService;
+        public MathServiceTests()
+        {
+            this.mathService = new MathService();
+        }
 
         [Test]
         public void Addition_Test()
         {
             //Aggange
-            MathService mathService = new MathService();
             var expected = 10;
+
             //Act
             var actual = mathService.Addition(5, 5);
+
             //Assert
             Assert.AreEqual(expected, actual);
         }
@@ -32,7 +32,6 @@ namespace CIMS.Service.Test
         public void Subtraction_Test()
         {
             //Arrenge
-            MathService mathService = new MathService();
             var expected = 2.2;
 
             //Act
@@ -46,7 +45,6 @@ namespace CIMS.Service.Test
         public void Multiplication_Test()
         {
             //Arrenge
-            MathService mathService = new MathService();
             var expected = 9.68;
 
             //Act
@@ -60,7 +58,6 @@ namespace CIMS.Service.Test
         public void Division_Test()
         {
             //Arrenge
-            MathService mathService = new MathService();
             var expected = 2;
 
             //Act
