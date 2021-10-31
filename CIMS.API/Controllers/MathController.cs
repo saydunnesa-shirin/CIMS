ed898f64ef1c90eba9fc494600bd6150a7d979f6
Shirin <Shirin0789@hotmail.com>
2021-10-31 13:42:55 +0200

    Math Test Added

--- CIMS.API/Controllers/MathController.cs
+++ CIMS.API/Controllers/MathController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using CIMS.Service;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -11,32 +12,38 @@ namespace CIMS.API.Controllers
     [ApiController]
     public class MathController : ControllerBase
     {
+        private readonly IMathService _mathService;
+        public MathController(IMathService mathService)
+        {
+            _mathService = mathService;
+        }
         [HttpPost]
         [Route("Addition")]
         public decimal Addition(decimal a, decimal b)
         {
-            return a + b;
+            return _mathService.Addition(a, b);
         }
 
         [HttpPost]
         [Route("Subtraction")]
         public decimal Subtraction(decimal a, decimal b)
         {
-            return a - b;
+            return _mathService.Subtraction(a, b);
         }
 
         [HttpPost]
         [Route("Multiplication")]
-        public decimal Multiplication(decimal a, decimal b)
+        public IActionResult Multiplication(decimal a, decimal b)
         {
-            return a * b;
+            var result = _mathService.Multiplication(a, b);
+            return Ok(result);
         }
 
         [HttpPost]
         [Route("Division")]
         public decimal Division(decimal a, decimal b)
         {
-            return a / b;
+            return _mathService.Division(a, b);
         }
     }
 }
